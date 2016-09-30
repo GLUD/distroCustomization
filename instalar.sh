ba02e2251edd848a843e406d56ecc525decec119
Diego Javier Mena Amado <ingelectronicadj@gmail.com>
2016-09-30 11:53:30 -0500

    Se arregla error de variable de entorno FPT a FTP

--- instalar.sh
+++ instalar.sh
@@ -14,7 +14,7 @@ echo '  > ^ < '
 
 #https://wiki.archlinux.org/index.php/proxy_settings
 function proxy {
-export {HTTP,HTTPS,FPT,ALL,SOCKS,RSYNC}_PROXY=http://10.20.4.15:3128
+export {HTTP,HTTPS,FTP,ALL,SOCKS,RSYNC}_PROXY=http://10.20.4.15:3128
 export {http,https,ftp,all,socks,rsync}_proxy=http://10.20.4.15:3128
 export {NO_PROXY,no_proxy}="localhost,127.0.0.1,localaddress,.localdomain.com,10.20.0.0/16"
 
@@ -22,7 +22,7 @@ env | grep -i proxy
 }
 
 function proxyoff {
-unset {HTTP,HTTPS,FPT,ALL,SOCKS,RSYNC,NO}_PROXY
+unset {HTTP,HTTPS,FTP,ALL,SOCKS,RSYNC,NO}_PROXY
 unset {http,https,ftp,all,socks,rsync,no}_proxy
 
 env | grep -i proxy 
