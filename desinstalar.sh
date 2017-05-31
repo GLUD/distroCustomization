1dfc889a860636d254c67f89e2c7c5e457e59d32
Jorge Ulises <naturalmentejorge@gmail.com>
2017-05-31 15:17:46 -0500

    Desinstalación para que quite la línea.

--- desinstalar.sh
+++ desinstalar.sh
@@ -5,3 +5,21 @@ sudoersfile=/etc/sudoers.d/glud
 
 # rationale: esta es una desinstalación parcial, falta quitar de los .bashrc
 sudo rm -rf $aplicacion $sudoersfile
+
+
+# rationale: se elimina una línea en los .bashrc para quitar el source al glud.sh
+# para el futuro se planea hacerlo para /etc/profile.d ?
+archivos=(
+~/.bashrc
+"/root/.bashrc"
+)
+
+for i in "${archivos[@]}"
+do
+  if sudo grep -i "$aplicacion" $i &> /dev/null
+  then
+    sudo sed -i.bak "/source \/usr\/local\/bin\/glud.sh/d" $i
+  else
+    echo "El archivo $i ya está modificado."
+  fi
+done
