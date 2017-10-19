3d5ac8fa99520e093fb944432a69b775c40b01d2
Jorge Ulises <naturalmentejorge@gmail.com>
2017-10-19 17:12:59 -0500

    Se agrega status y configuración mediante variable de entorno

--- instalar.sh
+++ instalar.sh
@@ -12,11 +12,22 @@ echo "  /\\_/\\  Hola $usuario"
 echo ' ( o.o ) Bienvenid@ al Grupo GNU/Linux UD '
 echo '  > ^ < '
 
-#https://wiki.archlinux.org/index.php/proxy_settings
+# rationale: se una una única variable de entorno para establecer el proxy
+# si no existe se pone una predeterminada
+if [ -z "$PROXY_DIR" ]; then
+  PROXY_DIR=http://10.20.4.15:3128
+fi
+
+# rationale: agrega todas las variables de entorno conocidas
+# si desea agregar proxy a aplicaciones, puede generar nuevas funciones
+# que validen la existencia del programa y establezcan su proxy con alias
+# u otras técnicas (falta agregar un ejemplo)
+# link: https://www.arin.net/knowledge/address_filters.html
+# link: https://wiki.archlinux.org/index.php/proxy_settings
 function proxy {
-export {HTTP,HTTPS,FTP,ALL,SOCKS,RSYNC}_PROXY=http://10.20.4.15:3128
-export {http,https,ftp,all,socks,rsync}_proxy=http://10.20.4.15:3128
-export {NO_PROXY,no_proxy}="localhost,127.0.0.1,localaddress,.localdomain.com,10.20.0.0/16"
+export {HTTP,HTTPS,FTP,ALL,SOCKS,RSYNC}_PROXY=$PROXY_DIR
+export {http,https,ftp,all,socks,rsync}_proxy=$PROXY_DIR
+export {NO_PROXY,no_proxy}="localhost,127.0.0.1,localaddress,.localdomain.com,10.0.0.0/8,192.168.0.0/16,172.16.0.0/12"
 
 env | grep -i proxy
 }
@@ -25,6 +36,10 @@ function proxyoff {
 unset {HTTP,HTTPS,FTP,ALL,SOCKS,RSYNC,NO}_PROXY
 unset {http,https,ftp,all,socks,rsync,no}_proxy
 
+env | grep -i proxy
+}
+
+function proxystat {
 env | grep -i proxy
 }
 EOF
@@ -79,5 +94,5 @@ fi
 
 # rationale: Mostrar al usuario qué hay que hacer
 echo
-echo 'Ejecuta el comando:'
+echo 'Para usar ejecuta el comando:'
 echo "source $aplicacion"
