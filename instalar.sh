c8ef1704302de7826930fabe252114093c2d9992
Jorge Ulises Useche Cuellar <naturalmentejorge@gmail.com>
2018-04-21 11:54:26 -0500

    Se modifica para mejor organización, se valida que funciona.

--- instalar.sh
+++ instalar.sh
@@ -36,50 +36,67 @@ else
 fi
 }
 
+# rationale: agrega el proxy para el comando "npm"
+# link: https://stackoverflow.com/questions/21228995/how-to-clear-https-proxy-setting-of-npm
+# link: https://stackoverflow.com/questions/25660936/using-npm-behind-corporate-proxy-pac
 function proxy_npm {
-if [ "$1" != "off" ]; then
-  if ! type "$npm" &> /dev/null; then
-    #remove proxy npm
-    npm config delete proxy -g
-    npm config delete http-proxy -g
-    npm config delete https-proxy -g
-  fi
-else
-  # Install proxy in npm
-  if ! type "$npm" &> /dev/null; then
+if npm --version &> /dev/null
+then
+  if [ "$1" != "off" ]
+  then
+    # set global proxy npm
     npm config set proxy $PROXY_DIR
     npm config set http-proxy $PROXY_DIR
     npm config set https-proxy $PROXY_DIR
+  else
+    # remove global proxy npm
+    npm config delete proxy
+    npm config delete http-proxy
+    npm config delete https-proxy
   fi
+  npm config list | grep -i proxy
 fi
 }
 
+# rationale: agrega proxy a la terminal de bash mediante variables de entorno
+# además excluye ciertas IP's de tener el dominio, como las de loopback e intranet
+function proxy_bash {
+  if [ "$1" != "off" ]
+  then
+    export {HTTP,HTTPS,FTP,ALL,SOCKS,RSYNC}_PROXY=$PROXY_DIR
+    export {http,https,ftp,all,socks,rsync}_proxy=$PROXY_DIR
+    export {NO_PROXY,no_proxy}="localhost,127.0.0.1,localaddress,.localdomain.com,10.0.0.0/8,192.168.0.0/16,172.16.0.0/12"
+  else
+    unset {HTTP,HTTPS,FTP,ALL,SOCKS,RSYNC,NO}_PROXY
+    unset {http,https,ftp,all,socks,rsync,no}_proxy
+  fi
+  env | grep -i proxy
+}
 
-# rationale: agrega todas las variables de entorno conocidas
+# rationale: muestra al usuario el estado actual del proxy
+# ponga aquí los demás testigos del cambio del proxy en su comando o entorno
+function proxy_status {
+  env | grep -i proxy
+  npm config list | grep -i proxy
+}
+
+# rationale: agrega proxy a todo lo conocido y usado en el glud
 # si desea agregar proxy a aplicaciones, puede generar nuevas funciones
 # que validen la existencia del programa y establezcan su proxy con alias
-# u otras técnicas (falta agregar un ejemplo)
+# u otras técnicas, hay varios ejemplos puestos
 # link: https://www.arin.net/knowledge/address_filters.html
 # link: https://wiki.archlinux.org/index.php/proxy_settings
 function proxy {
-export {HTTP,HTTPS,FTP,ALL,SOCKS,RSYNC}_PROXY=$PROXY_DIR
-export {http,https,ftp,all,socks,rsync}_proxy=$PROXY_DIR
-export {NO_PROXY,no_proxy}="localhost,127.0.0.1,localaddress,.localdomain.com,10.0.0.0/8,192.168.0.0/16,172.16.0.0/12"
-proxy_apt_key
-proxy_npm
-env | grep -i proxy
+  proxy_bash
+  proxy_apt_key
+  proxy_npm
 }
 
+# rationale: desactivar el proxy a las aplicaciones y entornos anteriores
 function proxyoff {
-unset {HTTP,HTTPS,FTP,ALL,SOCKS,RSYNC,NO}_PROXY
-unset {http,https,ftp,all,socks,rsync,no}_proxy
-proxy_apt_key off
-proxy_npm off
-env | grep -i proxy
-}
-
-function proxystat {
-env | grep -i proxy
+  proxy_bash off
+  proxy_apt_key off
+  proxy_npm off
 }
 
 EOF
@@ -139,17 +156,17 @@ EOF
 
   # rationale: algunos alias para gestor de paquetes apt
   sudo tee -a $aplicacion << 'EOF'
-# rationale: alias apt
+# rationale: alias para comando apt
 if apt --version &> /dev/null
 then
-alias aref='sudo apt update'
-alias aup='sudo apt upgrade'
-alias ain='sudo apt install'
-alias ase='apt search'
-alias arm='sudo apt remove'
-alias aarm='sudo apt autoremove'
-
+  alias aref='sudo apt update'
+  alias aup='sudo apt upgrade'
+  alias ain='sudo apt install'
+  alias ase='apt search'
+  alias arm='sudo apt remove'
+  alias aarm='sudo apt autoremove'
 fi
+
 EOF
 
 fi
